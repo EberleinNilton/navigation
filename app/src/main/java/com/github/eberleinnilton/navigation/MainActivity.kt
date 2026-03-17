0acdf0d997d9860d00ae174167a69d3bb08c3eb7
Eberlein Nilton <macarronenilton02@gmail.com>
2026-03-17 16:20:14 -0300

    criar tela home

--- app/src/main/java/com/github/eberleinnilton/navigation/MainActivity.kt
+++ app/src/main/java/com/github/eberleinnilton/navigation/MainActivity.kt
@@ -3,8 +3,10 @@ package com.github.eberleinnilton.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +26,13 @@ fun AppNavigation() {
         navController = navController,
         startDestination = "home"
     ) {
-
+        composable("home") {
+            HomeScreen()
+        }
     }
+}
+
+@Composable
+fun HomeScreen() {
+    Text("Tela Home")
 }
