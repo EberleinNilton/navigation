5bb5e4cb5744116c83d1bd3dfd302c3872277dab
Eberlein Nilton <macarronenilton02@gmail.com>
2026-03-17 16:19:02 -0300

    criar estrutura de navegação

--- app/src/main/java/com/github/eberleinnilton/navigation/MainActivity.kt
+++ app/src/main/java/com/github/eberleinnilton/navigation/MainActivity.kt
@@ -3,13 +3,27 @@ package com.github.eberleinnilton.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello")
+            AppNavigation()
         }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+
     }
 }
